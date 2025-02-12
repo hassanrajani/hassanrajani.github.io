e0f0efac91ca9f9081fe5e3b54f2026363167291
Hassan Rajani <hassan2112d@aptechgdn.net>
2025-02-12 17:37:13 +0500

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -30,7 +30,7 @@ const Navbar = () => {
           <NavLink href='#education'>Education</NavLink>
         </NavItems>
         <ButtonContainer>
-          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          <GitHubButton href='https://github.com/hassanrajani/' target="_blank">Github Profile</GitHubButton>
         </ButtonContainer>
         {
           isOpen &&
